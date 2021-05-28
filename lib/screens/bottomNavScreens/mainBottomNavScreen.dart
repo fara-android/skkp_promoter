fc25c36ee8c7526cf8bb31590d6aaba3e3335e01
ATursunov777 <a.tursunov1210@gmail.com>
2021-05-28 11:18:30 +0600

    add mainBottomScreenExOne

--- lib/screens/bottomNavScreens/mainBottomNavScreen.dart
+++ lib/screens/bottomNavScreens/mainBottomNavScreen.dart
@@ -10,6 +10,68 @@ class MainBottomNavScreen extends StatefulWidget {
 class _MainBottomNavScreenState extends State<MainBottomNavScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Main Screen",),),);
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.only(top: 80, right: 17, bottom: 0, left: 17),
+          color: Colors.white, // поменять на res color (mainBackGroundColor)!
+          child: Column(
+            children: [
+              Container(
+                child: Row(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(8),
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.green,
+                      ),
+                      child: Image.asset("assets/images/sim.png"),
+                    ),
+                    SizedBox(
+                      width: 12,
+                    ),
+                    Text(
+                      "СКПП\nПРОМОУТЕР",
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ), //поменять на res(appName)!!!
+                    SizedBox(
+                      width: 46,
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 75),
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        "ПРОФИЛЬ", //  Скорее всего придеться ставить имендж с фигмы
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 44,
+              ),
+              Container(
+                child: Row(
+                  children: [
+                    Text("Продажа SIM Карт "),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
