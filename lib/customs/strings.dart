4b58018782c8818f2667c1da71e3f822fd20092a
ATursunov777 <a.tursunov1210@gmail.com>
2021-06-03 13:59:13 +0600

    add tarifDetailScreen

--- lib/customs/strings.dart
+++ lib/customs/strings.dart
@@ -22,3 +22,7 @@ String currentTarrifText = "Тариф на SIM-карте: ";
 String weekText = "/нед";
 String monthText = "/мес";
 String availableText = "Доступные тарифы";
+String tariffDefinition =
+    "Описание тарифа текст супер интернет бесплатно но если скачаешь себе приложение МЕГА USSD";
+String connectTarifText = "Подключить ТАРИФ";
+String tarifConnectCallText = "*342*123124412*123412*23#";
--- lib/screens/simSellScreens/tariffDetailScreen.dart
+++ lib/screens/simSellScreens/tariffDetailScreen.dart
@@ -13,60 +13,237 @@ class TarifDetailScreen extends StatefulWidget {
 }
 
 class _TarifDetailScreenState extends State<TarifDetailScreen> {
+  createConnectionTarifPopUp(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(
+              "Подключение тарифа",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.green),
+            ),
+            content: Container(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text("Подтвердите правильность выбранного тарифа и номера"),
+                  SizedBox(),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        alignment: Alignment.center,
+                        child: Text(
+                          "ОТМЕНА",
+                          style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 14,
+                              color: Colors.black),
+                        ),
+                        padding: EdgeInsets.only(right: 32, bottom: 0),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Container(
+                        padding: EdgeInsets.only(right: 18, bottom: 0),
+                        child: Text("ПОТВЕРДИТЬ",
+                            style:
+                                TextStyle(fontSize: 14, color: Colors.green)),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          );
+        });
+  }
+
   TarrifList list = new TarrifList();
   @override
   Widget build(BuildContext context) {
+    double mediaQ = MediaQuery.of(context).size.height;
     return Scaffold(
+      backgroundColor: mainBackGroundColor,
       body: SafeArea(
-              child: SingleChildScrollView(
-          child: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(height: 12,),
-                Row(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      icon: Icon(Icons.arrow_back_ios, color: Colors.green),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      }),
+                  Text(
+                    widget.getNum,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(width: 4),
+                  Container(
+                      padding: EdgeInsets.only(top: 6),
+                      child: Text(numActiveText,
+                          style: TextStyle(color: Colors.green, fontSize: 12))),
+                  SizedBox(width: 4)
+                ],
+              ),
+              SizedBox(
+                height: mediaQ / 50,
+              ),
+              Container(
+                padding: EdgeInsets.only(right: 140),
+                child: Text(
+                  list.listOfTarrifs[widget.index].tariffName,
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                height: mediaQ / 23,
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 16, right: 16),
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    IconButton(
-                        icon: Icon(Icons.arrow_back_ios, color: Colors.green),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        }),
-                    Text(
-                      widget.getNum,
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(width: 4),
-                    Container(
-                        padding: EdgeInsets.only(top: 6),
-                        child: Text(numActiveText,
-                            style: TextStyle(color: Colors.green, fontSize: 12))),
-                    SizedBox(width: 4)
+                    createContainerDetail(
+                        'assets/images/payment.png',
+                        list.listOfTarrifs[widget.index].price.toString(),
+                        "с/нед."),
+                    createContainerDetail(
+                        'assets/images/phone.png',
+                        list.listOfTarrifs[widget.index].minutesToOtherOper
+                            .toString(),
+                        "мин/нед."),
                   ],
                 ),
-                SizedBox(
-                  height: 35,
+              ),
+              SizedBox(
+                height: mediaQ / 50,
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 16, right: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    createContainerDetail(
+                        'assets/images/internet.png',
+                        list.listOfTarrifs[widget.index].gb.toString(),
+                        "ГБ/нед."),
+                    createContainerDetail(
+                        'assets/images/call_in1.png',
+                        list.listOfTarrifs[widget.index].insidemMega.toString(),
+                        "мин/нед"),
+                  ],
                 ),
-                Container(
-                  padding: EdgeInsets.only(right: 140),
-                  child: Text(
-                    list.listOfTarrifs[widget.index].tariffName,
-                    style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: mediaQ / 50,
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 16, right: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    createContainerDetail(
+                        'assets/images/Sms1.png',
+                        list.listOfTarrifs[widget.index].sms.toString(),
+                        "нед."),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: mediaQ / 23,
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 16, right: 16),
+                child: Text(
+                  tariffDefinition,
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+              SizedBox(
+                height: mediaQ / 8,
+              ),
+              InkWell(
+                onTap: () {
+                  createConnectionTarifPopUp(context);
+                },
+                child: Container(
+                  padding: EdgeInsets.only(right: 16, left: 16),
+                  child: Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.green),
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      connectTarifText.toUpperCase(),
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
                   ),
                 ),
-                SizedBox(
-                  height: 28,
-                ),
-                Row(
-                  children: [],
-                ),
-              ],
-            ),
+              ),
+              SizedBox(
+                height: mediaQ / 50,
+              ),
+              Text(tarifConnectCallText),
+            ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget createContainerDetail(String images, String numText, String period) {
+    return Container(
+      width: MediaQuery.of(context).size.width / 2.35,
+      padding: EdgeInsets.all(18),
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.2),
+          spreadRadius: 2,
+          blurRadius: 4,
+          offset: Offset(0, 3),
+        ),
+      ], borderRadius: BorderRadius.circular(8), color: Colors.white),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            images,
+            height: 30,
+            width: 30,
+          ),
+          Text(
+            numText,
+            style: TextStyle(
+                color: Colors.green, fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            period,
+          )
+        ],
       ),
     );
   }
